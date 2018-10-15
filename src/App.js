509b8ef005aba85ce200f8185995c11606e522c0
m5r <me@mokhtarmial.com>
2018-10-15 15:46:13 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,28 +1,83 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import gonzalez from './gonzalez.png';
+
+function postKilled(amount) {
+	return fetch(`http://localhost:3029/kill/${amount}`, { method: 'POST' });
+}
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	state = {
+		end: Date.now() + 20900,
+		interval: null,
+		killed: '',
+	};
+
+	componentWillMount() {
+		const interval = setInterval(
+			() => this.update(),
+			250,
+		);
+		this.setState({ interval });
+	}
+
+	update() {
+		if (Date.now() > this.state.end) {
+			clearInterval(this.state.interval);
+			this.setState({ interval: null, killed: 'five' });
+			postKilled('five');
+			localStorage.setItem('killed', 'five');
+		}
+
+		this.forceUpdate();
+	}
+
+	click = () => {
+		if (this.state.interval) {
+			clearInterval(this.state.interval);
+			this.setState({ interval: null, killed: 'one' });
+			postKilled('one');
+			localStorage.setItem('killed', 'one');
+		}
+	};
+
+	render() {
+		const { end, interval, killed } = this.state;
+		const timeLeft = end - Date.now();
+		const finishedText = killed === 'one' ? 'You killed one.' : 'You let five die.';
+		const timeToDisplay = interval ? `${Math.floor(timeLeft / 1000)}s` : finishedText;
+
+		/* eslint-disable jsx-a11y/alt-text */
+
+		return (
+			<div className='App'>
+				<header>
+					{timeToDisplay}
+				</header>
+				<main className='App-main'>
+					<div className={`top ${killed === 'one' && 'killed'}`}>
+						<img src={gonzalez} />
+					</div>
+					<p>
+						Would you kill one <i>Speedy Gonzalez</i> to save five ?
+					</p>
+					<div className={`bottom ${killed === 'five' && 'killed'}`}>
+						<img src={gonzalez} />
+						<img src={gonzalez} />
+						<img src={gonzalez} />
+						<img src={gonzalez} />
+						<img src={gonzalez} />
+					</div>
+				</main>
+				<footer className={!interval && !!killed ? 'hidden' : ''}>
+					<div className='center'>
+						<div className='select-button' onClick={this.click} />
+					</div>
+					Let five die or kill one.
+				</footer>
+			</div>
+		);
+	}
 }
 
 export default App;
